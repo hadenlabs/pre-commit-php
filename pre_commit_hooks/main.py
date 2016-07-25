7012f667628a3ed201223381caed186f1d7ba25e
@slovacus <slovacus@gmail.com>
2016-07-24 23:09:57 -0500

    [#2] Fixed code file validator

--- pre_commit_hooks/main.py
+++ pre_commit_hooks/main.py
@@ -7,56 +7,37 @@ Author:
     Luis Alberto Mayta
 
 """
-
 from __future__ import print_function
 import argparse
 import sys
 
+WARNING_MSG = 'File {0} line: {1} codigo: {2}'
 
-def check_file_validator(file_name):
-    """Check Text files"""
-
-    functions_black_list = (
-        'eval',
-        'var_dump',
-        'print_r',
-    )
-
-    list_lines = []
-
-    for line, value in enumerate(file_name):
-        result = [i for i in functions_black_list if i in value]
-        if result:
-            list_lines.append(
-                (file_name, line, value,)
-            )
-
-    return list_lines
+FUNCTIONS_BLACK_LIST = (
+    'eval',
+    'var_dump',
+    'print_r',
+)
 
 
-def main():
-    """ Main functions handling configuration files etc"""
+def detect_function_not_allowed(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*')
+    args = parser.parse_args(argv)
 
-    parser = argparse.ArgumentParser(description='Process validation file.')
-    parser.add_argument('file', type=argparse.FileType('r'),
-                        help='file to validation')
-    args = parser.parse_args()
+    retcode = 0
 
-    result = check_file_validator(file_name=args.file)
-    if result:
-        print('Se encontraron los siguientes Errores')
-        for error in result:
-            print('file: {0} line: {1} codigo: {2}'.format(error[0],
-                                                           error[1],
-                                                           error[2]))
-        message = ('If you are sure you want to commit',
-                   'those files,',
-                   'use --no-verify option',)
-        print(' '.join(message))
-        sys.exit(1)
+    for filename in args.filenames:
+        with open(filename, 'rb') as inputfile:
+            for line, value in enumerate(inputfile):
+                result = [i for i in FUNCTIONS_BLACK_LIST if i in value]
+                if result:
+                    print(WARNING_MSG.format(
+                        inputfile, line, value,
+                    ))
+                    retcode = 1
+    return retcode
+
 
 if __name__ == '__main__':
-    main()
-    sys.exit(0)
-
-sys.exit(1)
+    sys.exit(detect_function_not_allowed())
